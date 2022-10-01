0b11d19a751d8a5f21d9488644e3750622eaf951
unknown <perezoscar360@gmail.com>
2022-10-01 02:35:38 -0500

    Finished password generator

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # -3-JavaScript-challenge
+
+## My first CSS project
+This is my challenge for module three, it's a password generator. The purpose of this project was for the user to trust the webpage therefore creating a custom webpage based off their answers on a series of questions. First time using JavaScript 
+
+## Motivation
+My second week into bootcamp we spoke about JavaScript. In order to get one step closer to achieving my goals, I used my motivation to test my skills and apply this language. This project took my computaion thinking to a whole nother level.
+
+## Build status
+I am positive that this webpage is completed and fully interactive. Although in the future I will add lots of exciting new features!
+
+## Credits
+functions with mozilla https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+
+## License
+MIT © [Oscar Perez-Hernandez]()
--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uperCaseChar = [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+var possibleChar = []
+function generatePassword() {
+  var finalPassword = []
+  var passwordLength = prompt("How many characters do you want this password to be?")
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Not valid Length")
+    return
+  }
+  console.log(passwordLength);
+  var special = confirm("do you want special characters?")
+  var lowerCase = confirm("do you want lower case characters?")
+  var uperCase = confirm("do you want uper case characters?")
+  var numeric = confirm("do you want numeric characters?")
+  console.log(specialChar, lowerCaseChar, uperCaseChar, numericChar);
+
+  if (special) {
+   possibleChar = possibleChar.concat (specialChar)
+ } 
+  if (lowerCase) {
+    possibleChar = possibleChar.concat (lowerCaseChar)
+  }
+  if (uperCase) {
+    possibleChar = possibleChar.concat (uperCaseChar)
+}
+if (numeric) {
+  possibleChar = possibleChar.concat (numericChar)
+}
+
+for (let index = 0; index < passwordLength; index++) {
+  finalPassword.push(possibleChar[Math.floor(Math.random() * possibleChar.length)])
+}
+
+return finalPassword.join("")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
